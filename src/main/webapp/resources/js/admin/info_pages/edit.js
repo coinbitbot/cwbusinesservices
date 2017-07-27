db28be2d5596dbe466590f58a71740b02b64621d
borsch <oleh.kurpiak@gmail.com>
2017-07-27 20:06:50 +0300

    insert image tool

--- src/main/webapp/resources/js/admin/info_pages/edit.js
+++ src/main/webapp/resources/js/admin/info_pages/edit.js
@@ -5,6 +5,19 @@
     app.controller('edit_forms', function($scope, $http) {
         tinymce.init({
             selector:'#info_page_text',
+            relative_urls : false,
+            remove_script_host : false,
+            convert_urls : true,
+            plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste imagetools"
+            ],
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+            content_css: [
+                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                '//www.tinymce.com/css/codepen.min.css'
+            ],
             height: 400
         });
 
