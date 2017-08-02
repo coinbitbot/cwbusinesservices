a456bfee10c7c8311a67bbb48e9ec8a4d0f5f013
Andrii Glybovets <andriy@glybovets.com.ua>
2017-08-02 19:04:10 +0300

    delete validation

--- src/main/java/com/cwbusinesservices/services/industry/IndustryValidationServiceImpl.java
+++ src/main/java/com/cwbusinesservices/services/industry/IndustryValidationServiceImpl.java
@@ -1,16 +1,30 @@
 package com.cwbusinesservices.services.industry;
 
+import com.cwbusinesservices.convertors.Fields;
+import com.cwbusinesservices.criteria.Criteria;
+import com.cwbusinesservices.criteria.impl.RequestCriteria;
 import com.cwbusinesservices.exceptions.BaseException;
+import com.cwbusinesservices.exceptions.service_error.ForbiddenException;
 import com.cwbusinesservices.pojo.entities.IndustryEntity;
+import com.cwbusinesservices.pojo.entities.RequestEntity;
 import com.cwbusinesservices.pojo.enums.PermissionsEnum;
 import com.cwbusinesservices.services.BaseValidator;
+import com.cwbusinesservices.services.request.IRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Andrii on 29.07.2017.
  */
 @Service
 public class IndustryValidationServiceImpl extends BaseValidator<IndustryEntity, Integer> implements IIndustryValidationService {
+
+    @Autowired
+    private IRequestService requestService;
+
     public IndustryValidationServiceImpl(){
         super(PermissionsEnum.CREATE_INDUSTRY,PermissionsEnum.EDIT_INDUSTRY,PermissionsEnum.DELETE_INDUSTRY,IndustryEntity.class);
     }
@@ -18,6 +32,12 @@ public class IndustryValidationServiceImpl extends BaseValidator<IndustryEntity,
     @Override
     public void validForDelete(IndustryEntity entity) throws BaseException {
         super.validForDelete(entity);
-        //TODO CHECK REQUESTS IF ANY HAVE INDUSTRY
+        RequestCriteria criteria = new RequestCriteria();
+        List<Integer> ids = new ArrayList<>();
+        ids.add(entity.getId());
+        criteria.setIndustry_ids(ids);
+        Integer requestEntities =requestService.count(criteria);
+        if (requestEntities!=0)
+            throw new ForbiddenException();
     }
 }
--- src/main/java/com/cwbusinesservices/services/interests/InterestValidationServiceImpl.java
+++ src/main/java/com/cwbusinesservices/services/interests/InterestValidationServiceImpl.java
@@ -1,17 +1,27 @@
 package com.cwbusinesservices.services.interests;
 
+import com.cwbusinesservices.criteria.impl.RequestCriteria;
 import com.cwbusinesservices.exceptions.BaseException;
 import com.cwbusinesservices.exceptions.service_error.ForbiddenException;
 import com.cwbusinesservices.pojo.entities.InterestEntity;
 import com.cwbusinesservices.pojo.enums.PermissionsEnum;
 import com.cwbusinesservices.services.BaseValidator;
+import com.cwbusinesservices.services.request.IRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Andrii on 29.07.2017.
  */
 @Service
 public class InterestValidationServiceImpl extends BaseValidator<InterestEntity,Integer> implements IInterestValidationService{
+
+    @Autowired
+    private IRequestService requestService;
+
     public InterestValidationServiceImpl(){
         super(PermissionsEnum.CREATE_INTEREST,PermissionsEnum.EDIT_INTEREST,PermissionsEnum.DELETE_INTEREST, InterestEntity.class);
     }
@@ -19,6 +29,12 @@ public class InterestValidationServiceImpl extends BaseValidator<InterestEntity,
     @Override
     public void validForDelete(InterestEntity entity) throws BaseException {
         super.validForDelete(entity);
-        //TODO CHECK REQUESTS IF ANY HAVE INTEREST
+        RequestCriteria criteria = new RequestCriteria();
+        List<Integer> ids = new ArrayList<>();
+        ids.add(entity.getId());
+        criteria.setInterest_ids(ids);
+        Integer requestEntities =requestService.count(criteria);
+        if (requestEntities!=0)
+            throw new ForbiddenException();
     }
 }
